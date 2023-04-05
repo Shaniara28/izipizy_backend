2920c6f3575ae77b162745a0d31536c0a5ba8df1
Hosealeonardo18 <hosealeonardo18@gmail.com>
2023-04-05 09:10:48 +0700

    test cors

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
   // }
   // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
   // res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-  // next()
+  next()
 })
 
 app.use("/api/v1", mainRouter)
