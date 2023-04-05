38718a7626994e5d41639e4cafb5a8534926bb0c
Hosealeonardo18 <hosealeonardo18@gmail.com>
2023-04-05 08:45:09 +0700

    solve cors

--- index.js
+++ index.js
@@ -19,10 +19,6 @@ app.use(helmet())
 app.use(xss())
 
 app.use((req, res, next) => {
-  const allowedOrigins = ["https://mama-recipe-izipizy.vercel.app"]
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin)
-  }
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
