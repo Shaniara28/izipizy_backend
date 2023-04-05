2dcc97ea14fad8ebb7fafb3759edf891cff4f5a8
Hosealeonardo18 <hosealeonardo18@gmail.com>
2023-04-05 08:55:25 +0700

    solve cors

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ app.use(morgan("dev"))
 app.use(helmet())
 app.use(xss())
 
-app.use((req, res, next) => {
-  const allowedOrigins = ["https://mama-recipe-izipizy.vercel.app"]
-  const origin = req.headers.origin
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin)
-  }
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-  next()
-})
+// app.use((req, res, next) => {
+//   const allowedOrigins = ["https://mama-recipe-izipizy.vercel.app"]
+//   const origin = req.headers.origin
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin)
+//   }
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+//   next()
+// })
 
 app.use(cors({
   methods: ["GET", "PUT", "POST", "DELETE"]
