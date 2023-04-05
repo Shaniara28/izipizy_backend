a029299ae24e7915f940b59e68f365580a03b01e
Hosealeonardo18 <hosealeonardo18@gmail.com>
2023-04-05 15:43:48 +0700

    benerin lagi corsnya

--- index.js
+++ index.js
@@ -40,13 +40,6 @@ app.all("*", (req, res, next) => {
   next(new createError.NotFound())
 })
 
-app.use((err, req, res, next) => {
-  const messageError = err.message || "internal server error"
-  const statusCode = err.status || 500
-  res.status(statusCode).json({
-    message: messageError,
-  })
-})
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
