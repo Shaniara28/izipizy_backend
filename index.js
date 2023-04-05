07a4b06d296b94b769eceddefe5469dcfc647e18
Hosealeonardo18 <hosealeonardo18@gmail.com>
2023-04-05 15:42:29 +0700

    localhost cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 const port = process.env.PORT
 
 app.use(cors({
-  origin: ['https://mama-recipe-izipizy.vercel.app', 'http://localhost:3000/login'],
+  origin: ['http://localhost:3000/login'],
   methods: "GET, PUT, POST, DELETE",
   credentials: true
 }))
