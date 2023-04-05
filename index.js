0a2c40030f62d6c57de1f7249457726389744ff8
Hosealeonardo18 <hosealeonardo18@gmail.com>
2023-04-05 09:03:06 +0700

    coba solve cors

--- index.js
+++ index.js
@@ -11,27 +11,29 @@ const mainRouter = require("./routes/index")
 const app = express()
 const port = process.env.PORT
 
-app.use(cors())
+const corsOptions = {
+  origin: 'https://mama-recipe-izipizy.vercel.app',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 app.options('*', cors())
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(helmet())
 app.use(xss())
 
-// app.use((req, res, next) => {
-//   const allowedOrigins = ["https://mama-recipe-izipizy.vercel.app"]
-//   const origin = req.headers.origin
-//   if (allowedOrigins.includes(origin)) {
-//     res.setHeader("Access-Control-Allow-Origin", origin)
-//   }
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-//   next()
-// })
-
-app.use(cors({
-  methods: ["GET", "PUT", "POST", "DELETE"]
-}));
+app.use((req, res, next) => {
+  const allowedOrigins = ["https://mama-recipe-izipizy.vercel.app"]
+  const origin = req.headers.origin
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin)
+  }
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  next()
+})
 
 app.use("/api/v1", mainRouter)
 
